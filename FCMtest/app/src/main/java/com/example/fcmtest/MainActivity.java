2f5e01a2a3fe4039118650778ffaeeea3ce020f2
Sukhesh Nuthalapati <sukhesh.nuthalapati@ncr.com>
2020-06-19 16:27:31 -0400

    adding button changing text

--- FCMtest/app/src/main/java/com/example/fcmtest/MainActivity.java
+++ FCMtest/app/src/main/java/com/example/fcmtest/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.fcmtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    int oldValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        changeTextViewValueRandomlyOnButtonClick();
+
     }
+    private void changeTextViewValueRandomlyOnButtonClick() {
+
+        final String[] alphabet = {"a","b","c"};
+        final TextView changingText = (TextView) findViewById((R.id.text_to_change));
+        Button changeTextButton = (Button) findViewById(R.id.change_text_button);
+
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int random = (int) (Math.random() * alphabet.length);
+                changingText.setText(alphabet[random]);
+                oldValue = random;
+            }
+        });
+        System.out.println(changingText.getText());
+    }
+
 }
