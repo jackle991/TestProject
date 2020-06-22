ecd2cff116b15f2978c97ca285e6698e58a01f31
aurora36 <67164417+aurora36@users.noreply.github.com>
2020-06-21 22:10:01 -0400

    Adding a button to send message to a phone number

--- FCMtest/app/src/main/java/com/example/fcmtest/MainActivity.java
+++ FCMtest/app/src/main/java/com/example/fcmtest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.fcmtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
@@ -12,6 +16,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     int oldValue;
+    private EditText editTextNumber;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         changeTextViewValueRandomlyOnButtonClick();
 
+        //asking specific permissions
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+            Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS
+        }, PackageManager.PERMISSION_GRANTED);
+
+        editTextMessage = findViewById(R.id.editTextTextMultiLine);
+        editTextNumber = findViewById(R.id.editTextNumber);
     }
+
     private void changeTextViewValueRandomlyOnButtonClick() {
 
         final String[] alphabet = {"a","b","c","d","e","f","g"};
@@ -37,4 +51,12 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(changingText.getText());
     }
 
+    //onclick will show all the public methods in this file
+    public void sendSMS (View view) {
+        String message = editTextMessage.getText().toString();
+        String number = editTextNumber.getText().toString();
+
+        SmsManager mySmsManager = SmsManager.getDefault();
+        mySmsManager.sendTextMessage(number, null, message, null, null );
+    }
 }
